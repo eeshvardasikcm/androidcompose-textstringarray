baa5d33fec5d9c00fa0db3cba51bd3e0bc209fc1
Erik Douglas Ward <contactme@eeshvardasikcm.com>
2022-07-22 18:19:14 -0700

    Adding textstringarray...

--- src/androidTest/java/com/eeshvardasikcm/textstringarray/ExampleInstrumentedTest.java
+++ src/androidTest/java/com/eeshvardasikcm/textstringarray/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.eeshvardasikcm.textstringarray.test", appContext.getPackageName());
+        assertEquals("error", "com.eeshvardasikcm.textstringarray.test", appContext.getPackageName());
     }
 }
